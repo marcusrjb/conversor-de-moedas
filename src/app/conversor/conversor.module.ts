8df1abc34f5dbbc962389cf0a7d6a70e31bbdf73
Marcus Britto <marcus.britto@gmail.com>
2020-09-25 20:05:42 -0300

    Aula 103

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
-
+import { ConversorComponent } from './components';
+import { MoedaService, ConversorService } from './services';
 
 @NgModule({
-  declarations: [],
+  declarations: [ConversorComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [
+  	ConversorComponent
+  ],
+  providers: [
+  	MoedaService,
+    ConversorService
   ]
 })
 export class ConversorModule { }
